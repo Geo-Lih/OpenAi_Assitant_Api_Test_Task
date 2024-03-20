320d6c7a2f23676aa75100f787d68a00f0daa6c4
HLikhitchenko <Lihitchenkozhorik@gmail.com>
2024-03-21 01:54:18 +0200

    remove: SECRET_KEY/ALGORITHM from config

--- config.py
+++ config.py
@@ -23,7 +23,5 @@ ROLE = 'user'
 # AUTH config
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v0/auth/login")
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b1a9563b93f7099f6f0f4caa6cf63b88e8d3e8"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
